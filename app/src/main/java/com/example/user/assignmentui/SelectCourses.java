a9265776cdf8d316f661698ff0fd6da7462ca9f3
User <User@DESKTOP-C308M1J>
2017-11-21 06:05:22 +0800

    mobile assignmemt

---
+++ app/src/main/java/com/example/user/assignmentui/SelectCourses.java
@@ -0,0 +1,13 @@
+package com.example.user.assignmentui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectCourses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_courses);
+    }
+}
---
+++ app/src/main/java/com/example/user/assignmentui/chatting.java
@@ -0,0 +1,13 @@
+package com.example.user.assignmentui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chatting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+    }
+}
